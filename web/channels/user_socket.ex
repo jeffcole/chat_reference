ff428744fc377fe8c95d37e3134c712edf3f7b4d
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-12-06 08:48:25 -0700

    Prepare for Heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule BoulderElixirChat.UserSocket do
   channel "chat:lobby", BoulderElixirChat.ChatChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
